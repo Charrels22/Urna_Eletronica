0c7f5c873a316808016e43f82ee33032d5892ec3
Charrels22 <charrels22@outlook.com>
2022-09-20 23:55:09 -0300

    modified

--- etapas.js
+++ etapas.js
@@ -1,35 +1,5 @@
 let etapas = [
     {
-        titulo: 'DEPUTADO ESTADUAL',
-        numeros: 5,
-        candidatos:[
-            {
-                numero: '12345',
-                nome: 'AATROX',
-                partido: 'RUNETERRA',
-                fotos: [
-                    {url: '12345.jpg', legenda: 'DEPUTADO ESTADUAL'}
-                ]
-            },
-            {
-                numero: '12346',
-                nome: 'BLITZCRANK',
-                partido: 'ZAUN',
-                fotos: [
-                    {url: '12346.jpg', legenda: 'DEPUTADO ESTADUAL'}
-                ]
-            },
-            {
-                numero: '12347',
-                nome: 'CASSIOPEIA',
-                partido: 'NOXUS',
-                fotos: [
-                    {url: '12347.jpg', legenda: 'DEPUTADA ESTADUAL'}
-                ]
-            },
-        ]
-    },
-    {
         titulo: 'DEPUTADO FEDERAL',
         numeros: 4,
         candidatos:[
@@ -49,72 +19,12 @@ let etapas = [
                     {url: '1235.jpg', legenda: 'DEPUTADA FEDERAL'}
                 ]
             },
-            {
-                numero: '1236',
-                nome: 'FIORA',
-                partido: 'DEMACIA',
-                fotos: [
-                    {url: '1236.jpg', legenda: 'DEPUTADA FEDERAL'}
-                ]
-            },
-        ]
-    },
-    {
-        titulo: 'SENADOR',
-        numeros: 3,
-        candidatos:[
-            {
-                numero: '123',
-                nome: 'GANGPLANK',
-                partido: 'ÁGUAS DE SENTINA',
-                suplente1: 'GRAVES',
-                suplente2: 'ILLAOI',
-                fotos: [
-                    {url: '123.jpg', legenda: 'SENADOR'},
-                    //{url: '123_2.jpg', legenda: '1° SUPLENTE', small: true},
-                    //{url: '123_3.jpg', legenda: '2° SUPLENTE', small: true}
-                ]
-            },
-            {
-                numero: '124',
-                nome: 'HEIMERDINGER',
-                partido: 'PILTOVER',
-                suplente1: 'JAYCE',
-                suplente2: 'CAITLYN',
-                fotos: [
-                    {url: '124.jpg', legenda: 'SENADOR'},
-                    // {url: '124_2.jpg', legenda: '1° SUPLENTE', small: true},
-                    // {url: '124_3.jpg', legenda: '2° SUPLENTE', small: true}
-                ]
-            },
-            {
-                numero: '125',
-                nome: 'IVERN',
-                partido: 'IONIA',
-                suplente1: 'IRELIA',
-                suplente2: 'KARMA',
-                fotos: [
-                    {url: '125.jpg', legenda: 'SENADOR'},
-                    // {url: '125_2.jpg', legenda: '1° SUPLENTE', small: true},
-                    // {url: '125_3.jpg', legenda: '2° SUPLENTE', small: true}
-                ]
-            },
         ]
     },
     {
         titulo: 'GOVERNADOR',
         numeros: 2,
         candidatos:[
-            {
-                numero: '12',
-                nome: 'JARVAN IV',
-                partido: 'DEMACIA',
-                vicegovernador: 'JAX',
-                fotos: [
-                    {url: '12.jpg', legenda: 'GOVERNADOR'},
-                    {url: '12_2.jpg', legenda: 'VICE-GOVERNADOR', small: true}
-                ]
-            },
             {
                 numero: '13',
                 nome: "KAI'SA",
@@ -141,16 +51,6 @@ let etapas = [
         titulo: 'PRESIDENTE',
         numeros: 2,
         candidatos: [
-            {
-                numero: '20',
-                nome: 'LEONA',
-                partido: 'TARGON',
-                vice: 'SHEN',
-                fotos: [
-                    {url: '20.jpg', legenda: 'PRESIDENTE'},
-                    {url: '20_2.jpg', legenda: 'VICE-PRESIDENTE', small: true}
-                ]
-            },
             {
                 numero: '21',
                 nome: 'BRAND',
